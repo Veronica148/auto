ed1eae6f5a6b5a37fd8262e8d0edf37c1f95c6ce
Veronica Lapunka <Veronica.Lapunka@viacomcontractor.com>
2018-08-15 17:29:51 +0300

    New files for work with rq/rs

---
+++ src/test/java/com/auto/common/models/provider/ProvidersList.java
@@ -0,0 +1,7 @@
+package com.auto.common.models.provider;
+
+/**
+ * Created by veronica_lapunka on 8/15/18.
+ */
+public class ProvidersList {
+}
---
+++ src/test/java/com/auto/test/tests/RequestResponseTest.java
@@ -0,0 +1,7 @@
+package com.auto.test.tests;
+
+/**
+ * Created by veronica_lapunka on 8/13/18.
+ */
+public class RequestResponseTest {
+}
