4374dfebd23e7e881249625e45962a737071009b
Veronica148 <veronica_lapunka@epam.com>
2019-07-27 11:15:53 +0300

    Added absolute path to chrome.exe

--- src/test/java/com/auto/common/driver/ChromeDriverManager.java
+++ src/test/java/com/auto/common/driver/ChromeDriverManager.java
@@ -1,7 +1,6 @@
 package com.auto.common.driver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class ChromeDriverManager extends DriverManager{
@@ -9,10 +8,14 @@ public class ChromeDriverManager extends DriverManager{
 
     @Override
     protected void createDriver() {
-        String exePath = "d:\\Veronica\\Work\\git\\auto\\src\\test\\resources\\drivers\\chromedriver.exe";
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        String exePath = classLoader.getResource("drivers/chromedriver.exe").getPath();
         System.setProperty("webdriver.chrome.driver", exePath);
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
+
 }
